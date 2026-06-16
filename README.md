23d7429b56e1cb18f94cdd3f9b7b7e1c5b679eaa
kabonka <kennyq18@outlook.com>
2026-06-16 08:47:59 +0800

    Update NPI Dashboard 2026-06-16 08:47

--- README.md
+++
@@ -1,1 +0,0 @@
-# Force redeploy Mon May 11 15:37:55 CST 2026
--- auto_update.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-chcp 65001 >nul
-set PYTHONIOENCODING=utf-8
-"C:\Users\msipm\AppData\Local\Programs\Python\Python313\python.exe" "C:\npi-dashboard\auto_update.py"
